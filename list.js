ebc9655b9bf4b02daab01f2f6367a54417407edb
Yvel-tal42-86 <77574839+Yvel-tal42-86@users.noreply.github.com>
2021-03-29 17:00:01 +0530

    removed
     getElementById() for anilist letters

--- list.js
+++ list.js
@@ -1,35 +1,4 @@
 
- /*var AnimeFirstletter = {
-    A: document.getElementById('A'),
-    B: document.getElementById('B'),
-    C: document.getElementById('C'),
-    D: document.getElementById('D'),
-    E: document.getElementById('E'),
-    F: document.getElementById('F'),
-    G: document.getElementById('G'),
-    H: document.getElementById('H'),
-    I: document.getElementById('I'),
-    J: document.getElementById('J'),
-    K: document.getElementById('K'),
-    L: document.getElementById('L'),
-    M: document.getElementById('M'),
-    N: document.getElementById('N'),
-    O: document.getElementById('O'),
-    P: document.getElementById('P'),
-    Q: document.getElementById('Q'),
-    R: document.getElementById('R'),
-    S: document.getElementById('S'),
-    T: document.getElementById('T'),
-    U: document.getElementById('U'),
-    V: document.getElementById('V'),
-    W: document.getElementById('W'),
-    X: document.getElementById('X'),
-    Y: document.getElementById('Y'),
-    Z: document.getElementById('Z'),
-    NON: document.getElementById('0-9')
-};
-*/
-
 {/* <script type="text/javascript" src="list.js"></script> */}
 // <!--Displays Date-->
 // <script type="text/javascript">
